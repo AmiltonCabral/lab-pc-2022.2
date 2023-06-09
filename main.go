e60b5cb4687d5169d8750f0649cfc59e0a669c01
Joabcmp <joabcmp@gmail.com>
2023-06-09 12:17:22 -0300

    HTTP 1.1 response e request

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	srv := &http.Server{Addr: ":8000", Handler: http.HandlerFunc(handle)}
+
+	log.Printf("Serving on https://0.0.0.0:8000")
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got connection: %s", r.Proto)
+	w.Write([]byte("Hello"))
+}
