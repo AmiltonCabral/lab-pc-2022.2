a30577e522d09086ad06d8fb4a1efea37e70520f
Iagohss <iago.silva@ccc.ufcg.edu.br>
2023-06-09 17:18:45 -0300

    work in progress

--- src/main.go
+++ src/main.go
@@ -1,30 +1,43 @@
 package main
 
 import (
-	//"math/rand"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
-func http2Server(outPutCh chan string, jCh chan int) {
-	for x := range outPutCh {
-		fmt.Printf(x + "\n")
+func http2Server(ch chan string, jCh chan int) {
+	tick := time.Tick(1000 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("Se passaram 100 milisegundos")
+		case x := <-ch:
+			if x == "Me informe seu status" {
+				fmt.Println("httpstatus.200")
+			}
+		default:
+			close(jCh)
+			return
+		}
 	}
-
-	close(jCh)
 }
 
-func http2Clients(inputCh chan string) {
+func http2Clients(ch chan string) {
 	x := rand.Intn(40)
 	for i := 0; i < x; i++ {
-		inputCh <- "httpStatus.200"
-	}
-	close(inputCh)
+		select {
+		case ch <- "Me informe seu status":
+		default:
+			fmt.Println("Servidor lotado. Tente mais tarde")
+		}
 
+	}
+	close(ch)
 }
 
 func main() {
-	ch := make(chan string, 20)
+	ch := make(chan string, 5)
 	joinCh := make(chan int)
 
 	go http2Server(ch, joinCh)
