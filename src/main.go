76d46627a0a29e7c44c22e1cbe702993aab14848
Iagohss <iago.silva@ccc.ufcg.edu.br>
2023-06-09 16:41:58 -0300

    feat: criado funções htt2Server e htt2Clients

--- src/main.go
+++ src/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	//"math/rand"
 	"fmt"
+	"math/rand"
 )
 
-func http2() string {
-	return "httpStatus.200"
+func http2Server(outPutCh chan string, jCh chan int) {
+	for x := range outPutCh {
+		fmt.Printf(x + "\n")
+	}
+
+	close(jCh)
+}
+
+func http2Clients(inputCh chan string) {
+	x := rand.Intn(40)
+	for i := 0; i < x; i++ {
+		inputCh <- "httpStatus.200"
+	}
+	close(inputCh)
+
 }
 
 func main() {
-	fmt.Printf(http2()+"\n")
+	ch := make(chan string, 20)
+	joinCh := make(chan int)
+
+	go http2Server(ch, joinCh)
+	go http2Clients(ch)
+
+	<-joinCh
 }
