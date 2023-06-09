84cdcae9f1942211c63f7d75c1ffe0dda372ef6b
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2023-06-09 15:57:22 -0300

    feat: go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AmiltonCabral/projeto-pc-2022.2
+
+go 1.20
---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func http2() string {
+	return "httpStatus.200"
+}
+
+func main() {
+	fmt.Printf(http2()+"\n")
+}
